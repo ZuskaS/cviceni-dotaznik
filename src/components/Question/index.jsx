34cf7de07e47f155c61e50f056f63f5d6b10262c
Zuzana Ilečková <zuzka.ileckova@gmail.com>
2025-05-13 22:59:18 +0200

    rozpracování úkolu 5 dotazník s fajfkou

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -2,19 +2,20 @@ import { QuestionBody } from '../QuestionBody/index';
 import { Option } from '../Option/index';
 import './style.css';
 
-export const Question = (props) => {
+export const Question = (text) => {
   return (
     <div className="question">
-      <QuestionBody 
-        iconType="symbolQuestion" 
-        text={props.text}
-      />
+      <QuestionBody iconType="symbolQuestion" text={text} />
+      <div
+        className="option"
+        onClick={() => console.log('spíše souhlasím')}
+      ></div>
       <div className="question__options">
-        <Option type="smileyStrongYes" text="Souhlasím"/>
-        <Option type="smileyYes" text="Spíše souhlasím"/>
-        <Option type="smileyNeutral" text="Nevím"/>
-        <Option type="smileyNo" text="Spíše nesouhlasím"/>
-        <Option type="smileyStrongNo" text="Nesouhlasím"/>
+        <Option type="smileyStrongYes" text="Souhlasím" />
+        <Option type="smileyYes" text="Spíše souhlasím" />
+        <Option type="smileyNeutral" text="Nevím" />
+        <Option type="smileyNo" text="Spíše nesouhlasím" />
+        <Option type="smileyStrongNo" text="Nesouhlasím" />
       </div>
     </div>
   );
